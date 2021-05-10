111948bdca4fe7914866fac641b3dddc46afccc6
Anton Bokovets <anton@radlogics.com>
2021-05-10 18:40:08 +0300

    lesson-15

---
+++ lesson-15/docker-entrypoint-exec/Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3.8.5
+COPY server.py /server.py
+ENTRYPOINT ["python3","-u", "server.py"]
--- lesson-15/docker/server-default.py
+++ lesson-15/docker/server-default.py
@@ -1,7 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socket
 import sys 
-import time
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -9,20 +8,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write(b'<b>Hello from hostname:</b> ' + socket.gethostname().encode() + b'<br><br>')
-        self.wfile.write(b'<b>Interval: </b> ' + str(interval).encode() + b'<br><br>')
-        self.wfile.write(b'<b>Desired count of print: </b> ' + str(desired_count).encode() + b'<br><br>')
         self.wfile.write(b'<b>Text arg: </b> ' + str(string_arg).encode() + b'<br><br>')
-        count = 1
-        while(count <= desired_count):
-            self.wfile.write(b"<b>" + str(count).encode() + b".</b> " + b"<b>Current time: </b>" + str(time.strftime("%X")).encode() + b"<br>")
-            time.sleep(interval)
-            count+=1
-        self.wfile.write(b"<br><b>End of loop.</b>")
-            
-interval = int(sys.argv[1])
-desired_count = int(sys.argv[2])
-string_arg = sys.argv[3]
 
+string_arg = sys.argv[1]
 SERVER_PORT = 8000
 httpd = HTTPServer(('0.0.0.0', SERVER_PORT), SimpleHTTPRequestHandler)
 print('Listening on port %s ...' % SERVER_PORT)
